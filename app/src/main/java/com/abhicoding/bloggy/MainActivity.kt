04c5d5f8faac92a795ea96905fe7e52ea9beaedd
abhinandan-verma <abhinandanverma551@gmail.com>
2024-01-05 11:21:17 +0530

    LaunchedEffect

--- app/src/main/java/com/abhicoding/bloggy/MainActivity.kt
+++ app/src/main/java/com/abhicoding/bloggy/MainActivity.kt
@@ -1,44 +1,65 @@
 package com.abhicoding.bloggy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.abhicoding.bloggy.ui.theme.BloggyTheme
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            Counter()
         }
     }
 }
 
 @Composable
-fun App() {
-    val theme = remember { mutableStateOf(false) }
-    BloggyTheme(theme.value) {
-        Column (
-            Modifier.background(MaterialTheme.colorScheme.background)
-        ){
-            Text(
-                text = "Abhinandan",
-                style = MaterialTheme.typography.bodyMedium
+fun Counter() {
+    val count = remember {
+        mutableIntStateOf(0)
+    }
+    var key = count.intValue % 3 == 0
+    LaunchedEffect(key1 = key){
+        Log.i("TAG", "Counter value: ${count.intValue}")
+    }
+    Button(
+        onClick = { count.intValue++ },
+    ) {
+        Text(
+            text = "Increment Count",
+            Modifier.background(
+                Brush.linearGradient(
+                    listOf(Color.Cyan, Color.Magenta, Color.Yellow)
+                )
             )
-            Button(onClick = {
-                theme.value = !theme.value
-            }) {
-                Text(text = "Change Theme")
-            }
+        )
+    }
+}
+/*
+@Composable
+fun ListComposable(){
+    val categoryState = remember { mutableStateOf(emptyList<String>()) }
+    LaunchedEffect(key1 = Unit){
+        categoryState.value = fetchCategories()
+    }
+    LazyColumn{
+        items(categoryState.value){item ->
+            Text(text = item)
         }
     }
 }
+fun fetchCategories(): List<String> {
+    return listOf("One", "Two","Three")
+}
+*/
